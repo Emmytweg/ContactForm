24dfdc80df850af85051512d2d60d8b32f23aeb6
Emmytweg <109160680+Emmytweg@users.noreply.github.com>
2024-06-18 13:01:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,43 +10,79 @@ const error = document.querySelector('.error')
 const capchaInput = document.querySelector('#capchaInput')
 const generalEnqiryInput = document.querySelector('#generalEnqiryInput')
 const radioButtons = document.querySelectorAll('input[name="option"]')
+const form = document.querySelector('form')
+const emailInput = document.querySelector('#emailInput')
+const message = document.querySelector('#message')
 
-submitBtn.addEventListener('click', (e)=> {
-    e.preventDefault()
-    if (nameInput.value === '') {
-        lastNameError.innerHTML = '<p>This field is required</p>'
-        firstNameError.innerHTML = '<p>This field is required</p>'
-        emailError.innerHTML = '<p>This field is required</p>'
-        msgError.innerHTML = '<p>This field is required</p>'
+form.addEventListener('submit' , (e)=> {
+  e.preventDefault();
+  if (validateform()) {
+    alert('Form successfully Submitted');
+    form.reset();
+  }
+})
+function validateform() {
+  let isValid = true;
 
-        setTimeout(() => lastNameError.remove(), 3000)
+  //Validate FirstName
+  if (nameInput.value.trim() === '') {
+    firstNameError.innerHTML = '<p>This Field is required</p>'
+    lastNameError.innerHTML = '<p>This Field is required</p>'
+    error.classList.add('.error')
+    isValid = false
+    setTimeout(() => lastNameError.remove(), 3000)
         setTimeout(() => firstNameError.remove(), 3000)
-        setTimeout(() => emailError.remove(), 3000)
-        setTimeout(() => msgError.remove(), 3000)
-        setTimeout(() => quryError.remove(), 3000)
+  }else {
+    firstNameError.innerHTML = ''
+    lastNameError.innerHTML = ''
+  }
+  //Validate Email
+  const emailPattern = /^[a-zA-z0-9._%+-]+@[a-zA-z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if ((emailInput.value.trim() === '')) {
+    emailError.innerHTML = '<p>This Field is required </p>'
+    error.classList.add('.error') 
+    isValid = false
+           setTimeout(() => emailError.remove(), 3000)
 
-          error.classList.add('.error')
+  }else if( (!emailPattern.test(emailInput.value.trim()))){
+    emailError.innerHTML = '<p>Enter a valid email address</p>'
+    error.classList.add('.error') 
+    isValid = false
+           setTimeout(() => emailError.remove(), 3000)
+  }else {
+    emailError.innerHTML = ''
 
-    }
-   let selected= false
-   radioButtons.forEach(radio => {
-    if (radio.checked) {
-      selected = true
-    }
-})
-if (!selected) {
-  quryError.innerHTML = '<p>Please select a query type</p>'
-console.log('please select')
-}else{
-  quryError.innerHTML = ' '
+  }
+  //validate message
+  if (message.value.trim() === '') {
+    msgError.innerHTML = '<p>This Field is required</p>'
+    error.classList.add('.error')
+    isValid = false
+         setTimeout(() => msgError.remove(), 3000)
 
+  }else {
+    msgError.innerHTML = ''
+  }
+  let selected= false
+     radioButtons.forEach(radio => {
+      if (radio.checked) {
+        selected = true
+      }
+  })
+  if (!selected) {
+    quryError.innerHTML = '<p>Please select a query type</p>'
+  setTimeout(() => quryError.remove(), 3000)
+
+  }else{
+    quryError.innerHTML = ' '
+  
+  }
+    if (!capchaInput.checked) {
+      capchaError.innerHTML = '<p>To submit this form please consent to being contacted</p>'
+      setTimeout(() => capchaError.remove(), 3000)
+  
+    }else if(capchaInput.checked) {
+      capchaError.innerHTML = ''
+    } 
+    return isValid
 }
-  if (!capchaInput.checked) {
-    capchaError.innerHTML = '<p>To submit this form please consent to being contacted</p>'
-    setTimeout(() => capchaError.remove(), 3000)
-
-  }else if(capchaInput.checked) {
-    capchaError.innerHTML = ''
-  } 
-  document.querySelector('body').innerHTML = '<h1>Please refresh your page</h1>'
-})
